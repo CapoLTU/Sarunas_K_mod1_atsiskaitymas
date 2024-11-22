093ec762e24c301e5cf8d8650358e9501bff63eb
CapoLTU <kontautas.s@gmail.com>
2024-11-22 14:19:32 +0200

    Prideta prideta papildoma funkcija "Skonis" ir duomenu irasymas i faila "Skonis_ir_Kvapas.txt"

--- sarunas_k_mod1_atsiskaitymas/web_read.py
+++ sarunas_k_mod1_atsiskaitymas/web_read.py
@@ -7,7 +7,7 @@ listas1 =[]
 def check_time(a):
     return  datetime.now() < laikas_end
 
-def duino (laikas : int = 2):
+def duino (laikas : int = 1):
     laikas_end: datetime = datetime.now() + timedelta(seconds=laikas)
     listas = []
     response = get("https://www.duino.lt/64-valdikliai")
@@ -15,13 +15,13 @@ def duino (laikas : int = 2):
     tree = HTML(text)
 
     products = tree.xpath("//div[contains(@class, 'right-block')]")
-    for product in products :
-        if check_time(laikas_end) == True :
-            prod = product.xpath(".//a[contains(@class, 'product-name')]/text()")
-            listas.append(prod)
-        else:
-            print("END TIME")
-            break
+    listas = [
+        {'Produktas : ': product.xpath(".//a[contains(@class, 'product-name')]/text()")[0],
+         'Kaina : ': product.xpath(".//span[contains(@class, 'price product-price')]/text()")[0].strip().replace("€",
+                                                                                                                 "")
+         }
+        for product in products
+    ]
     for i in listas:
         if check_time(laikas_end) == True :
             open("Duino.txt", "a").write(str(i) + "\n")
@@ -33,7 +33,22 @@ def duino (laikas : int = 2):
 listas1 = duino()
 print(listas1)
 
+def skonis():
+    listas = []
+    response = get("https://www.skonis-kvapas.lt/kava")
+    text = response.text
+    tree = HTML(text)
 
+    products = tree.xpath("//div[contains(@class, 'products__item  ')]")
+    listas = [
+        {'Produktas : ': product.xpath(".//h2[contains(@class, 'products__item-title')]/text()")[0].strip().replace("\n", ""),
+         'Kaina : ': product.xpath(".//div[contains(@class, 'products__item-price')]/ins/text()")[0].strip().replace("\xa0", "").replace("€","")
+         }
+        for product in products
+    ]
+    for i in listas:
+            open("Skonis_ir_Kvapas.txt", "a").write(str(i) + "\n")
+            open("Skonis_ir_Kvapas.txt", "a").close()
 
-
+    return listas
 
