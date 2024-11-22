ec70d4fc4173c721340ad23a36aa85c78c6f4a4a
CapoLTU <kontautas.s@gmail.com>
2024-11-22 12:01:24 +0200

    Prideta irasymas i file.txt

--- sarunas_k_mod1_atsiskaitymas/web_read.py
+++ sarunas_k_mod1_atsiskaitymas/web_read.py
@@ -1,8 +1,14 @@
 from requests import get
 from lxml.etree import HTML
-
+from datetime import datetime, timedelta
+laikas_end: datetime = datetime.now() + timedelta(seconds=6)
 listas1 =[]
-def duino ():
+
+def check_time(a):
+    return  datetime.now() < laikas_end
+
+def duino (laikas : int = 2):
+    laikas_end: datetime = datetime.now() + timedelta(seconds=laikas)
     listas = []
     response = get("https://www.duino.lt/64-valdikliai")
     text = response.text
@@ -10,8 +16,24 @@ def duino ():
 
     products = tree.xpath("//div[contains(@class, 'right-block')]")
     for product in products :
-        prod = product.xpath(".//a[contains(@class, 'product-name')]/text()")
-        listas.append(prod)
+        if check_time(laikas_end) == True :
+            prod = product.xpath(".//a[contains(@class, 'product-name')]/text()")
+            listas.append(prod)
+        else:
+            print("END TIME")
+            break
+    for i in listas:
+        if check_time(laikas_end) == True :
+            open("Duino.txt", "a").write(str(i) + "\n")
+            open("Duino.txt", "a").close()
+        else :
+            print("END TIME")
+            break
     return listas
 listas1 = duino()
 print(listas1)
+
+
+
+
+
