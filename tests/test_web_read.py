6bd971ff1dc54629b449866484ca2e0f493627ec
CapoLTU <kontautas.s@gmail.com>
2024-11-23 22:47:07 +0200

    pakoreguotas readme ir pridetas testu rezultatu failas

--- tests/test_web_read.py
+++ tests/test_web_read.py
@@ -9,8 +9,3 @@ class TestWeb_read(unittest.TestCase):
         self.assertRaises(TypeError, crowl(1, 2, 20))
         self.assertRaises(TypeError, crowl(1, ".txt", '20'))
 
-    def test_check_time(self):
-        sek = timedelta(seconds=10)
-        sek1 = timedelta(seconds=0)
-        self.assertEqual(check_time(sek), True)
-        self.assertEqual(check_time(sek1), False)
